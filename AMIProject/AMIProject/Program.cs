8916cd0daa53ef399a1fd9e50a6c306e0f90e31d
Luka Maric <luka94ns@gmail.com>
2017-11-11 10:58:54 +0100

    Initial Commit

---
+++ AMIProject/AMIProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMIProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello boys and girl!!!");
+            Console.ReadKey();
+        }
+    }
+}
