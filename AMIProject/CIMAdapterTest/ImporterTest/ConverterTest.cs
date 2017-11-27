c7a21cc58c3008d30b4c96aecc92d966ce8b9f78
bratislav94 <bratislav94@gmail.com>
2017-11-27 17:46:30 +0100

    Tests for converter finished, line coverage 100%

--- AMIProject/CIMAdapterTest/ImporterTest/ConverterTest.cs
+++ AMIProject/CIMAdapterTest/ImporterTest/ConverterTest.cs
@@ -232,5 +232,239 @@ namespace CIMAdapterTest.ImporterTest
 
             Converter.PopulateTransformerEndProperties(te, resDesc, new ImportHelper(), report); // gid < 0
         }
+
+        [Test]
+        public void PopulateConnectivityNodeContainerPropertiesTest()
+        {
+            Init();
+            Converter.PopulateConnectivityNodeContainerProperties(null, resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 0);
+
+            Converter.PopulateConnectivityNodeContainerProperties(new AMIProfile.ConnectivityNodeContainer(), resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 0);
+
+            AMIProfile.ConnectivityNodeContainer cn = new AMIProfile.ConnectivityNodeContainer() { ID = "CN_1", MRID = "CN_1", Name = "CN" };
+            impHelp.DefineIDMapping(cn.ID, 1234567);
+            Converter.PopulateConnectivityNodeContainerProperties(cn, resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 2);
+        }
+
+        [Test]
+        public void PopulateGeographicalRegionPropertiesTest()
+        {
+            Init();
+            Converter.PopulateGeographicalRegionProperties(null, resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 0);
+
+            Converter.PopulateGeographicalRegionProperties(new AMIProfile.GeographicalRegion(), resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 0);
+
+            AMIProfile.GeographicalRegion geo = new AMIProfile.GeographicalRegion() { ID = "geo_1", MRID = "geo_1", Name = "Vojvodina" };
+            impHelp.DefineIDMapping(geo.ID, 1234567);
+            Converter.PopulateGeographicalRegionProperties(geo, resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 2);
+        }
+
+        [Test]
+        public void PopulateSubGeographicalRegionPropertiesTest()
+        {
+            Init();
+            Converter.PopulateSubGeographicalRegionProperties(null, resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 0);
+
+            Converter.PopulateSubGeographicalRegionProperties(new AMIProfile.SubGeographicalRegion(), resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 0);
+
+            AMIProfile.SubGeographicalRegion sgr = new AMIProfile.SubGeographicalRegion() { ID = "sub_geo1", MRID = "sub_geo1" };
+            AMIProfile.GeographicalRegion gr = new AMIProfile.GeographicalRegion() { ID = "geo1" };
+            sgr.Region = gr;
+            impHelp.DefineIDMapping(sgr.ID, 1234567);
+            impHelp.DefineIDMapping(sgr.Region.ID, 12345678);
+            Converter.PopulateSubGeographicalRegionProperties(sgr, resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 2);
+
+            Converter.PopulateSubGeographicalRegionProperties(sgr, resDesc, new ImportHelper(), report); // gid < 0
+        }
+
+        [Test]
+        public void PopulateEquipmentContainerPropertiesTest()
+        {
+            Init();
+            Converter.PopulateEquipmentContainerProperties(null, resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 0);
+
+            Converter.PopulateEquipmentContainerProperties(new AMIProfile.EquipmentContainer(), resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 0);
+
+            AMIProfile.EquipmentContainer eqc = new AMIProfile.EquipmentContainer() { ID = "eqC1", MRID = "eqC1" };
+            impHelp.DefineIDMapping(eqc.ID, 1234567);
+            Converter.PopulateEquipmentContainerProperties(eqc, resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 1);
+        }
+
+        [Test]
+        public void PopulateSubstationPropertiesTest()
+        {
+            Init();
+            Converter.PopulateSubstationProperties(null, resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 0);
+
+            Converter.PopulateSubstationProperties(new AMIProfile.Substation(), resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 0);
+
+            AMIProfile.Substation sub = new AMIProfile.Substation() { ID = "sub1", MRID = "sub1", Name = "Trafo 1" };
+            AMIProfile.SubGeographicalRegion gr = new AMIProfile.SubGeographicalRegion() { ID = "geo1" };
+            sub.Region = gr;
+            impHelp.DefineIDMapping(sub.ID, 1234567);
+            impHelp.DefineIDMapping(sub.Region.ID, 12345678);
+            Converter.PopulateSubstationProperties(sub, resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 3);
+
+            Converter.PopulateSubstationProperties(sub, resDesc, new ImportHelper(), report); // gid < 0
+        }
+
+        [Test]
+        public void PopulateTapChangerPropertiesTest()
+        {
+            Init();
+            Converter.PopulateTapChangerProperties(null, resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 0);
+
+            Converter.PopulateTapChangerProperties(new AMIProfile.TapChanger(), resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 0);
+
+            AMIProfile.TapChanger tapC = new AMIProfile.TapChanger() { ID = "sub1", MRID = "sub1", HighStep = 4, LowStep = 1, NeutralStep = 0, NormalStep = 1 };
+            impHelp.DefineIDMapping(tapC.ID, 1234567);
+            Converter.PopulateTapChangerProperties(tapC, resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 5);
+
+            Converter.PopulateTapChangerProperties(tapC, resDesc, new ImportHelper(), report); // gid < 0
+        }
+
+        [Test]
+        public void PopulateRatioTapChangerPropertiesTest()
+        {
+            Init();
+            Converter.PopulateRatioTapChangerProperties(null, resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 0);
+
+            Converter.PopulateRatioTapChangerProperties(new AMIProfile.RatioTapChanger(), resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 0);
+
+            AMIProfile.RatioTapChanger ratio = new AMIProfile.RatioTapChanger() { ID = "ratio1", MRID = "ratio1"};
+            AMIProfile.TransformerEnd trEnd = new AMIProfile.TransformerEnd() { ID = "trEnd_1" };
+            ratio.TransformerEnd = trEnd;
+            impHelp.DefineIDMapping(ratio.ID, 1234567);
+            impHelp.DefineIDMapping(ratio.TransformerEnd.ID, 12345678);
+            Converter.PopulateRatioTapChangerProperties(ratio, resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 2);
+
+            Converter.PopulateRatioTapChangerProperties(ratio, resDesc, new ImportHelper(), report); // gid < 0
+        }
+
+        [Test]
+        public void PopulateVoltageLevelPropertiesTest()
+        {
+            Init();
+            Converter.PopulateVoltageLevelProperties(null, resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 0);
+
+            Converter.PopulateVoltageLevelProperties(new AMIProfile.VoltageLevel(), resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 0);
+
+            AMIProfile.VoltageLevel volLevel = new AMIProfile.VoltageLevel() { ID = "volLevel1", MRID = "volLevel1" };
+            AMIProfile.BaseVoltage baseVol = new AMIProfile.BaseVoltage() { ID = "baseVol_1" };
+            AMIProfile.Substation sub = new AMIProfile.Substation() { ID = "sub_1" };
+            volLevel.BaseVoltage = baseVol;
+            volLevel.Substation = sub;
+            impHelp.DefineIDMapping(volLevel.ID, 1234567);
+            impHelp.DefineIDMapping(volLevel.BaseVoltage.ID, 12345678);
+            impHelp.DefineIDMapping(volLevel.Substation.ID, 34267);
+            Converter.PopulateVoltageLevelProperties(volLevel, resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 3);
+
+            Converter.PopulateVoltageLevelProperties(volLevel, resDesc, new ImportHelper(), report); // gid < 0
+        }
+
+        [Test]
+        public void PopulatePowerTransformerEndPropertiesTest()
+        {
+            Init();
+            Converter.PopulatePowerTransformerEndProperties(null, resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 0);
+
+            Converter.PopulatePowerTransformerEndProperties(new AMIProfile.PowerTransformerEnd(), resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 0);
+
+            AMIProfile.PowerTransformerEnd powerEnd = new AMIProfile.PowerTransformerEnd() { ID = "powerTransEnd_1", MRID = "powerTransEnd_1" };
+            AMIProfile.PowerTransformer powerTrans = new AMIProfile.PowerTransformer() { ID = "powerTrans_1" };
+            powerEnd.PowerTransformer = powerTrans;
+            impHelp.DefineIDMapping(powerEnd.ID, 1234567);
+            impHelp.DefineIDMapping(powerEnd.PowerTransformer.ID, 12345678);
+            Converter.PopulatePowerTransformerEndProperties(powerEnd, resDesc, impHelp, report);
+            Assert.AreEqual(resDesc.Properties.Count, 2);
+
+            Converter.PopulatePowerTransformerEndProperties(powerEnd, resDesc, new ImportHelper(), report); // gid < 0
+        }
+
+        [Test]
+        [TestCase(AMIProfile.UnitSymbol.P)]
+        [TestCase(AMIProfile.UnitSymbol.Q)]
+        [TestCase(AMIProfile.UnitSymbol.V)]
+        [TestCase(3)]
+        public void GetDMSUnitSymbolTest(AMIProfile.UnitSymbol unit)
+        {
+            FTN.Common.UnitSymbol ret;
+            switch (unit)
+            {
+                case AMIProfile.UnitSymbol.P:
+                    ret = Converter.GetDMSUnitSymbol(unit);
+                    Assert.AreEqual(FTN.Common.UnitSymbol.P, ret);
+                    break;
+                case AMIProfile.UnitSymbol.Q:
+                    ret = Converter.GetDMSUnitSymbol(unit);
+                    Assert.AreEqual(FTN.Common.UnitSymbol.Q, ret);
+                    break;
+                case AMIProfile.UnitSymbol.V:
+                    ret = Converter.GetDMSUnitSymbol(unit);
+                    Assert.AreEqual(FTN.Common.UnitSymbol.V, ret);
+                    break;
+
+                default:
+                    ret = Converter.GetDMSUnitSymbol(unit);
+                    Assert.AreEqual(FTN.Common.UnitSymbol.V, ret);
+                    break;
+            }
+        }
+
+        [Test]
+        [TestCase(AMIProfile.Direction.WRITE)]
+        [TestCase(AMIProfile.Direction.READWRITE)]
+        [TestCase(AMIProfile.Direction.READ)]
+        [TestCase(3)]
+        public void GetDMSDirectionTest(AMIProfile.Direction direction)
+        {
+            FTN.Common.Direction ret;
+            switch (direction)
+            {
+                case AMIProfile.Direction.WRITE:
+                    ret = Converter.GetDMSDirection(direction);
+                    Assert.AreEqual(FTN.Common.Direction.WRITE, ret);
+                    break;
+                case AMIProfile.Direction.READWRITE:
+                    ret = Converter.GetDMSDirection(direction);
+                    Assert.AreEqual(FTN.Common.Direction.READWRITE, ret);
+                    break;
+                case AMIProfile.Direction.READ:
+                    ret = Converter.GetDMSDirection(direction);
+                    Assert.AreEqual(FTN.Common.Direction.READ, ret);
+                    break;
+
+                default:
+                    ret = Converter.GetDMSDirection(direction);
+                    Assert.AreEqual(FTN.Common.Direction.READ, ret);
+                    break;
+            }
+        }
     }
 }
